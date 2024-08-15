ff9cc880499176ee18d2001e150549c5857c27fc
VasudevKrishnaX <ishitcyber@gmail.com>
2024-08-15 20:43:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ownerID: 'YOUR_OWNER_ID',
+  ownerID: '707593121328005140',
   token: process.env.TOKEN,
   embedColor: '#FF0000',
   geniusToken: '0CHr9jtPjR7EKraXUVv6__8ELmEwFeUwt5vYAhk-is_fGOq-10-epdMzESkMPUnf',
